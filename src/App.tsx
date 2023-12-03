c3b5d74b65149f1cdf470efbb94a7743e87f17d3
Lukibwoy <lukaszwiczynski@gmail.com>
2023-12-03 20:33:00 +0100

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-// App.tsx
-
-import './App.css';
-import Navbar from './components/Navbar';
+import './App.css'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
-  return (
-    <div className="bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-orange-900 via-amber-100 to-orange-900 min-h-screen ">
-      <Navbar />
-    </div>
-  );
+	return (
+		<div className="bg=white ">
+			<Navbar />
+			<Header />
+		</div>
+	)
 }
 
-export default App;
+export default App
