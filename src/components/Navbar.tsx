ab100abbac6a620541d48ca1ebdc314e3767ee00
Lukibwoy <lukaszwiczynski@gmail.com>
2024-01-29 17:59:04 +0100

    css mobile fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,12 +14,12 @@ const Navbar: React.FC = () => {
 	}
 
 	return (
-		<nav className="p-2 relative">
+		<nav className="md:p-0 p-2 relative">
 			{/* Desktop Navigation */}
 			<div className="hidden md:flex items-center justify-between">
 				<Logo />
 
-				<div className="md:space-x-20 xxl:space-x-40 text-2xl mr-20 font-semibold">
+				<div className="md:space-x-5 lg:space-x-20 xxl:space-x-40 text-2xl mr-20 font-semibold">
 					<a href="#" className="text-black">
 						Home
 					</a>
@@ -32,13 +32,13 @@ const Navbar: React.FC = () => {
 					<a href="#" className="text-black ">
 						Contact
 					</a>
-					<button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-50">
+					<button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline md:ml-20 lg:ml-50">
 						Sign Up
 					</button>
 				</div>
 			</div>
 
-			{/* Mobile Navigation */}
+			{/* Mobile Navigation */}	
 			<div className="md:hidden flex items-center justify-between">
 				<Logo />
 				<motion.button
